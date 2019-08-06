261ecf85cc419dbf726a72883b9e9a8cd30f8fec
taylus <brandon@legacy.codes>
2019-08-06 15:38:37 -0500

    Use ImageSharp's ToBase64String() + add VS solution

--- WebApplication1/Base64ImageTranslator.cs
+++ WebApplication1/Base64ImageTranslator.cs
@@ -1,6 +1,6 @@
 using System;
-using System.IO;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
 using SixLabors.ImageSharp.PixelFormats;
 
 namespace WebApplication1
@@ -12,18 +12,14 @@ namespace WebApplication1
     {
         public Image<Rgba32> FromBase64String(string base64ImageData)
         {
+            if (base64ImageData.StartsWith("data:")) base64ImageData = base64ImageData.Split(',')[1];
             var imageBytes = Convert.FromBase64String(base64ImageData);
             return Image.Load(imageBytes);
         }
 
         public string ToBase64String(Image<Rgba32> image)
         {
-            using (var stream = new MemoryStream())
-            {
-                image.SaveAsPng(stream);
-                var bytes = stream.ToArray();
-                return Convert.ToBase64String(bytes);
-            }
+            return image.ToBase64String(PngFormat.Instance);
         }
     }
 }
--- WebApplication1/wwwroot/js/load-image.js
+++ WebApplication1/wwwroot/js/load-image.js
@@ -1,43 +1,43 @@
 /* jshint esnext:true, browser:true, devel: true */
-window.onBlazorReady = function(dotNetObjectRef) {
-  "use strict";
-  var input = document.querySelector("input[type=file]");
-  if (!input) return;
+window.onBlazorReady = function (dotNetObjectRef) {
+    "use strict";
+    var input = document.querySelector("input[type=file]");
+    if (!input) return;
 
-  input.addEventListener("change", function() {
-    var file = input.files[0];
-    getBase64FileData(file).then(function(base64FileData) {
-      //console.log("JS: Image data loaded, sending to .NET via interop: " + base64FileData);
-      dotNetObjectRef.invokeMethodAsync("ProcessImage", base64FileData).then(function(base64Output) {
-        //console.log("JS: Got data back from .NET via interop: " + base64Output);
-        showBeforeAndAfter(base64FileData, base64Output);
-      });
-    });
-  }, false);
+    input.addEventListener("change", function () {
+        var file = input.files[0];
+        getBase64FileData(file).then(function (base64FileData) {
+            //console.log("JS: Image data loaded, sending to .NET via interop: " + base64FileData);
+            dotNetObjectRef.invokeMethodAsync("ProcessImage", base64FileData).then(function (base64Output) {
+                //console.log("JS: Got data back from .NET via interop: " + base64Output);
+                showBeforeAndAfter(base64FileData, base64Output);
+            });
+        });
+    }, false);
 
-  //return a promise to return the base64 content of the given file
-  function getBase64FileData(file) {
-    var reader = new FileReader();
-    return new Promise(function(resolve, reject) {
-      reader.addEventListener("load", function() {
-        var data = reader.result.split(",")[1];
-        resolve(data);
-      }, false);
-      reader.readAsDataURL(file);
-    });
-  }
-
-  function showBeforeAndAfter(beforeData, afterData) {
-    var beforeImg = document.querySelector("img#before");
-    if (beforeImg) {
-      beforeImg.style.display = "inline";
-      beforeImg.src = "data:image/png;base64, " + beforeData;
+    //return a promise to return the base64 content of the given file
+    function getBase64FileData(file) {
+        var reader = new FileReader();
+        return new Promise(function (resolve, reject) {
+            reader.addEventListener("load", function () {
+                var data = reader.result;
+                resolve(data);
+            }, false);
+            reader.readAsDataURL(file);
+        });
     }
 
-    var afterImg = document.querySelector("img#after");
-    if (afterImg) {
-      afterImg.style.display = "inline";
-      afterImg.src = "data:image/png;base64, " + afterData;
+    function showBeforeAndAfter(beforeData, afterData) {
+        var beforeImg = document.querySelector("img#before");
+        if (beforeImg) {
+            beforeImg.style.display = "inline";
+            beforeImg.src = beforeData;
+        }
+
+        var afterImg = document.querySelector("img#after");
+        if (afterImg) {
+            afterImg.style.display = "inline";
+            afterImg.src = afterData;
+        }
     }
-  }
 };
